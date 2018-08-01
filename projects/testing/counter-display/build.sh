e5b5484f04565cef8e14029ec431cec3d27bff42
LemonRhyme <841049+LemonRhyme@users.noreply.github.com>
2018-07-31 21:07:23 -0600

    Fixed time display bug

--- projects/testing/counter-display/build.sh
+++ projects/testing/counter-display/build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-PATH=$PATH:$(pwd)/../../tools
-time mk65.sh src bin ROM.BIN
+PATH=$PATH:$(pwd)/../../../tools
+time mk65.sh src bin AUTOEXEC.BIN
 read -n 1 -s -r -p "Press any key to continue"
 exit
